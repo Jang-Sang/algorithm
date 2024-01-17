9ba570b8ef11c1aa49d6cae6a1c1a7784981074f
Jang Sang <110034416+Jang-Sang@users.noreply.github.com>
2024-01-17 16:11:33 +0900

    [level 0] Title: 나머지 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.js
@@ -0,0 +1,9 @@
+// function solution(num1, num2) {
+//     var answer = -1;
+//     return answer;
+// }
+
+const solution = (num1, num2) => {
+    const answer = (num1 % num2);
+    return answer;
+}
