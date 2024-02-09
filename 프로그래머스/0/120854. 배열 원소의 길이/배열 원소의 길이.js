99fe5e1087accaad492b2c500fabdc0bd97c986d
Jang Sang <110034416+Jang-Sang@users.noreply.github.com>
2024-02-10 03:22:29 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -0,0 +1,9 @@
+// function solution(strlist) {
+//     var answer = [];
+//     return answer;
+// }
+
+const solution = (strlist) => {
+    // 내부 데이터의 길이를 출력
+    return strlist.map((el) => el.length)
+}
