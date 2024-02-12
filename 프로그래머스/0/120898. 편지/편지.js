631fce44a1e8917fa9df2f36dc9d1f086598c24a
Jang Sang <110034416+Jang-Sang@users.noreply.github.com>
2024-02-13 06:31:49 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.js
@@ -0,0 +1,10 @@
+// function solution(message) {
+//     var answer = 0;
+//     return answer;
+// }
+
+const solution = (message) => {
+    // 배열을 갯수 세고 곱하기 2하기
+    let answer = (message.length * 2);
+    return answer;
+}
