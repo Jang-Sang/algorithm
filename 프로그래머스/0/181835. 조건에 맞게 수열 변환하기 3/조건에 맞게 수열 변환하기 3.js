0a69743d4999570faf2e10203a2f326ded991ff3
Jang Sang <110034416+Jang-Sang@users.noreply.github.com>
2024-03-01 21:58:56 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 2.79 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -0,0 +1,12 @@
+// function solution(arr, k) {
+//     var answer = [];
+//     return answer;
+// }
+
+function solution(arr, k) {
+    if(k%2===1){
+        return arr.map(el=>el*k)
+    }else if(k%2===0){
+        return arr.map(el=>el+k)
+    }else false
+}
