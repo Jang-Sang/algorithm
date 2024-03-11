569989d387186934effc072e418dd2b83ab5b15c
Jang Sang <110034416+Jang-Sang@users.noreply.github.com>
2024-03-11 23:28:09 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.js
@@ -0,0 +1,13 @@
+// function solution(a, b, flag) {
+//     var answer = 0;
+//     return answer;
+// }
+
+
+const solution = (a,b,flag) =>{
+    if(flag){
+        return a+b
+    }else{
+    return a-b
+    }
+}
