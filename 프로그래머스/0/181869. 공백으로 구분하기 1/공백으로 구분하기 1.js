dee3916e8a2ed36845ee77960cf2257c5257fa34
Jang Sang <110034416+Jang-Sang@users.noreply.github.com>
2024-03-12 07:26:20 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.js
@@ -0,0 +1,10 @@
+// function solution(my_string) {
+//     var answer = [];
+//     return answer;
+// }
+
+const solution = (my_string) => {
+   let answer = [];
+    answer = my_string.split(' ');
+    return answer;
+}
