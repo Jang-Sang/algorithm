1b4e667ab4b2b205e72df4aaf1d28e64e54e3c1c
Jang Sang <110034416+Jang-Sang@users.noreply.github.com>
2024-03-25 07:53:30 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
@@ -0,0 +1,12 @@
+// function solution(num_list, n) {
+//     var answer = 0;
+//     return answer;
+// }
+
+const solution = (num_list, n) => {
+ if(num_list.includes(n)){
+       return 1
+   } else {
+      return 0
+   }
+}
